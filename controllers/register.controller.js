04e30ee9058127323893349d7d7e0b8ee6abd1f0
Abhiramnaidu1 <nkank001@odu.edu>
2021-03-18 00:17:58 -0400

    Update register.controller.js

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -2,18 +2,23 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var config = require('config.json');
-
+const nodemailer = require('nodemailer');
 router.get('/', function (req, res) {
     res.render('register');
 });
 
 router.post('/', function (req, res) {
+
     // register using api to maintain clean separation between layers
+    req.body.state="inactive";
     request.post({
         url: config.apiUrl + '/users/register',
         form: req.body,
         json: true
     }, function (error, response, body) {
+      console.log(response);
+      console.log("========================================================================================================");
+      console.log(body);
         if (error) {
             return res.render('register', { error: 'An error occurred' });
         }
@@ -25,10 +30,28 @@ router.post('/', function (req, res) {
                 lastName: req.body.lastName,
                 username: req.body.username
             });
+
+            console.log("email sent");
         }
 
         // return to login page with success message
-        req.session.success = 'Registration successful';
+        req.session.success = 'Check your mail';
+        var smtpTrans = nodemailer.createTransport({
+           service: 'Gmail',
+           auth: {
+            user: 'webprogram1234@gmail.com',
+            pass: 'Abc@1234'
+          }
+        });
+        console.log("email");
+        smtpTrans.sendMail({
+          from: 'webprogram1234@gmail.com',
+          to: req.body.username,
+          subject:'Account confirmation',
+          text:'Hi'+req.body.firstName+'\n\n'+'To confirm email press below link :\n\n' +
+            'http://localhost:8000/api/users/verifytoken/' +body.insertedIds[0] + '\n\n',
+
+        });
         return res.redirect('/login');
     });
 });
